1456fb8e79151b55ec239ff9ff31da4e99872912
jccsISC <silva.jccs18@gmail.com>
2022-01-04 01:16:28 -0600

    Threads

--- app/src/main/java/com/jccsisc/fundamentoscorutinas/BasesPrevias.kt
+++ app/src/main/java/com/jccsisc/fundamentoscorutinas/BasesPrevias.kt
@@ -1,5 +1,8 @@
 package com.jccsisc.fundamentoscorutinas
 
+import kotlin.concurrent.thread
+import kotlin.random.Random
+
 /****
  * Project: FundamentosCorutinas
  * From: com.jccsisc.fundamentoscorutinas
@@ -7,10 +10,26 @@ package com.jccsisc.fundamentoscorutinas
  * More info: https://www.facebook.com/juliocesar.camachosilva/
  * All rights reserved 2022.
  ***/
-
 fun main() {
     lambda()
+    threads()
 }
+
+fun threads() {
+    println(multiThread(2, 6))
+}
+
+fun multiThread(x: Int, y: Int): Int {
+    var result = 0
+    thread {
+        Thread.sleep(somTime())
+        result = x * y
+        println(result)
+    }
+    return  result
+}
+
+fun somTime(): Long = Random.nextLong(500, 3_000)
 
 fun lambda() {
     println(multi(2,3))
