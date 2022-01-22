8a97fedd5ed5fc7d4780af2b822201c9d728176e
jccsISC <silva.jccs18@gmail.com>
2022-01-22 08:04:58 -0600

    Cerrar Channel

--- app/src/main/java/com/jccsisc/fundamentoscorutinas/CorrutinasAvanzadas.kt
+++ app/src/main/java/com/jccsisc/fundamentoscorutinas/CorrutinasAvanzadas.kt
@@ -1,6 +1,7 @@
 package com.jccsisc.fundamentoscorutinas
 
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -16,7 +17,38 @@ import kotlinx.coroutines.runBlocking
 val countries = listOf("Santander", "CDMX", "Lima", "Buenos Aires", "Apatzingán", "Puerto Vallarta")
 
 fun main() {
-    basicChannel()
+//    basicChannel()
+    closeChannel()
+}
+
+fun closeChannel() {
+    runBlocking {
+        newTopic("Cerrar un Canal")
+        val channel = Channel<String>()
+        launch {
+            countries.forEach {
+                channel.send(it)
+                //if (it == "Lima") channel.close()
+                if (it == "Lima") {
+                    channel.close()
+                    return@launch
+                }
+            }
+            //channel.close()
+        }
+        //consumimos el channel
+        /*for (value in channel) {
+            println(value)
+        }*/
+
+        //esta es una mejor forma de consumir el canal que que no dependeriamos de un repeat con su tamaño específico
+        while (!channel.isClosedForReceive) {
+            //si el canal no está cerrado, consume
+            println(channel.receive())
+        }
+
+//        channel.consumeEach { println(it) } //otra forma de consumir el canal
+    }
 }
 
 fun basicChannel() {
